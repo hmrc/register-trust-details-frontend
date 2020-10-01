3a9cb741a72dd4a425927f0f704f7e3a65c4c8f7
Jay Nabonne <jaynabonne@gmail.com>
2020-10-01 13:39:59 +0100

    TRUS-3185: Bring over trust details status logic from trusts-frontend.

---
+++ test/utils/RegistrationProgressSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import java.time.LocalDate
+
+import base.SpecBase
+import models.Status.{Completed, InProgress}
+import pages.TrustDetailsStatus
+import pages.register.trust_details.WhenTrustSetupPage
+import uk.gov.hmrc.http.HeaderCarrier
+
+class RegistrationProgressSpec extends SpecBase {
+  implicit lazy val hc: HeaderCarrier = HeaderCarrier()
+
+  "Trust details section" must {
+
+    "render no tag" when {
+
+      "no status value in user answers" in {
+        val registrationProgress = injector.instanceOf[RegistrationProgress]
+
+        val userAnswers = emptyUserAnswers
+
+        registrationProgress.trustDetailsStatus(userAnswers) mustBe None
+      }
+    }
+
+    "render in-progress tag" when {
+
+      "user has entered when the trust was created" in {
+
+        val registrationProgress = injector.instanceOf[RegistrationProgress]
+
+        val userAnswers = emptyUserAnswers
+            .set(WhenTrustSetupPage, LocalDate.of(2010, 10, 10)).success.value
+
+        registrationProgress.trustDetailsStatus(userAnswers).value mustBe InProgress
+      }
+    }
+
+    "render complete tag" when {
+
+      "user answer has reached check-trust-details" in {
+
+        val registrationProgress = injector.instanceOf[RegistrationProgress]
+
+        val userAnswers = emptyUserAnswers
+          .set(WhenTrustSetupPage, LocalDate.of(2010, 10, 10)).success.value
+          .set(TrustDetailsStatus, Completed).success.value
+
+        registrationProgress.trustDetailsStatus(userAnswers).value mustBe Completed
+      }
+    }
+  }
+
+}
