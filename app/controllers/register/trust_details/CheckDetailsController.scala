50e412986c9cb3502de8a18f69d46816e19424ab
carrielies <carrielies@users.noreply.github.com>
2021-01-22 10:45:28 +0000

    TRUS-3322 Upgrade to Play 2.7

--- app/controllers/register/trust_details/CheckDetailsController.scala
+++ app/controllers/register/trust_details/CheckDetailsController.scala
@@ -26,7 +26,7 @@ import pages.register.trust_details.CheckDetailsPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.RegistrationsRepository
-import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.print.TrustDetailsPrintHelper
 import viewmodels.AnswerSection
 import views.html.register.trust_details.CheckDetailsView
