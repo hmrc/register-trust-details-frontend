f5dc5433dc1ada1217c861edc9d789f7ce3cf478
Ina Toni-Uebari <74660663+ina-tu@users.noreply.github.com>
2023-09-29 15:57:51 +0100

    DDCE-4614: updated character count component (#94)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.22.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.20.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion
@@ -14,11 +14,11 @@ object AppDependencies {
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
+    "org.scalatest"               %% "scalatest"                % "3.2.17",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.27",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.2.0",
+    "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
 
