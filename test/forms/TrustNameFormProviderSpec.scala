e72f96ad67aba48a05975d0e663576aa393436f0
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2025-08-29 12:34:40 +0100

    Addressed comments from Matt regarding Test case.

--- test/forms/TrustNameFormProviderSpec.scala
+++ test/forms/TrustNameFormProviderSpec.scala
@@ -18,18 +18,16 @@ package forms
 
 import forms.behaviours.StringFieldBehaviours
 import play.api.data.FormError
-import org.scalatest.matchers.must.Matchers._
-
 
 class TrustNameFormProviderSpec extends StringFieldBehaviours {
 
-  private val requiredKey = "trustName.error.required"
-  private val lengthKey   = "trustName.error.length"
-  private val maxLength   = 53
-  private val regexp      = "^[A-Za-z0-9 ,.()/&'-]*$"
-  private val invalidKey  = "trustName.error.invalidCharacters"
+  val requiredKey = "trustName.error.required"
+  val lengthKey = "trustName.error.length"
+  val maxLength = 53
+  val regexp = "^[A-Za-z0-9 ,.()/&'-]*$"
+  val invalidKey = "trustName.error.invalidCharacters"
 
-  private val form = new TrustNameFormProvider()()
+  val form = new TrustNameFormProvider()()
 
   ".value" must {
 
@@ -41,11 +39,12 @@ class TrustNameFormProviderSpec extends StringFieldBehaviours {
       stringsWithMaxLength(maxLength)
     )
 
-    "not bind strings longer than 53 characters" in {
-      val overMax = "A" * (maxLength + 1)
-      val result  = form.bind(Map(fieldName -> overMax))
-      result.errors mustBe List(FormError(fieldName, lengthKey, Seq(maxLength)))
-    }
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
 
     behave like mandatoryField(
       form,
@@ -56,9 +55,9 @@ class TrustNameFormProviderSpec extends StringFieldBehaviours {
     behave like fieldWithRegexpWithGenerator(
       form,
       fieldName,
-      regexp     = regexp,
-      generator  = stringsWithMaxLength(maxLength),
-      error      = FormError(fieldName, invalidKey, Seq(regexp))
+      regexp = regexp,
+      generator = stringsWithMaxLength(maxLength),
+      error = FormError(fieldName, invalidKey, Seq(regexp))
     )
 
     behave like nonEmptyField(
