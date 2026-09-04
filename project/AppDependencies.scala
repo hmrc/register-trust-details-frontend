ccef464ebe732b0f1e51983ccdc506f7fbce7782
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-09-04 13:30:11 +0100

    DDCE-9171 update play frontend to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   private val playBootstrapVersion = "10.8.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.11.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.13.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % playBootstrapVersion
